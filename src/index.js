1ac498277f25d5cfcf89ec34375a082be21013e6
Darien Bernal <darien.bernal@gmail.com>
2025-02-04 23:24:42 -0500

    Inserted and styled sub menu to page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// DOM Manipulation (Part Two)
+
 // Menu data structure
 var menuLinks = [
     { text: 'about', href: '/about' },
@@ -6,46 +8,58 @@ var menuLinks = [
     { text: 'account', href: '/account' },
   ];
 
-// Part 1: Getting Started
-// 1. Select and cache the <main> element in a variable named mainEl.
+// Select and cache the <main> element in a variable named mainEl.
 const mainEl = document.querySelector('main');
 
-// 2. Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+// Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
 mainEl.style.backgroundColor = "var(--main-bg)";
 
-// 3. Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+// Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
 const h1 = document.createElement('h1');
 
 h1.textContent = "DOM Manipulation"
 mainEl.appendChild(h1);
 
-// 4. Add a class of flex-ctr to mainEl.
+// Add a class of flex-ctr to mainEl.
 mainEl.classList.add("flex-ctr");
 
-// Part 2: Creating a Menu Bar
-// 1. Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+// Creating a Menu Bar
+// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
 const topMenuEl = document.getElementById("top-menu");
 
-// 2. Set the height of the topMenuEl element to be 100%.
+// Set the height of the topMenuEl element to be 100%.
 topMenuEl.style.height = "100%";
 
-// 3. Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
 
-// 4. Add a class of flex-around to topMenuEl.
+// Add a class of flex-around to topMenuEl.
 topMenuEl.classList.add("flex-around");
 
-// Part 3: Adding Menu Buttons
+// Adding Menu Buttons
 console.log(menuLinks);
-// 1. Iterate over the entire menuLinks array and for each "link" object:
+// Iterate over the entire menuLinks array and for each "link" object:
 menuLinks.forEach(function(link) {
-    // 2. Create an <a> element.
+    // Create an <a> element.
     const aItem = document.createElement('a');
-    // 3. On the new element, add an href attribute with its value set to the href property of the "link" object.
+    // On the new element, add an href attribute with its value set to the href property of the "link" object.
     aItem.setAttribute("href", link.href);
-    // 4. Set the new element's content to the value of the text property of the "link" object.
+    // Set the new element's content to the value of the text property of the "link" object.
     aItem.textContent = link.text;
-    // 5. Append the new element to the topMenuEl element.
+    // Append the new element to the topMenuEl element.
     topMenuEl.appendChild(aItem);
  
 });
+
+// Part 3: Creating the Submenu
+// Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
+const subMenuEl = document.getElementById('sub-menu');
+
+// Set the height subMenuEl element to be "100%".
+subMenuEl.style.height = "100%";
+
+// Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+
+// Add the class of flex-around to the subMenuEl element.
+subMenuEl.classList.add("flex-around");
